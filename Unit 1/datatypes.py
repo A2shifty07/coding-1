96244775673bb189d8dc808a3e9dbc3b570c7959
A2shifty07 <amaddox354@boyslatin.net>
2026-09-01 13:37:14 +0000

    updated notes

---
+++ Unit 1/datatypes.py
@@ -0,0 +1,29 @@
+# string chracters that are wrapped within quotes chacters can bel letters, numbers, and symbols you can use both single and double quotation marks for strings 
+" ahmad maddox "
+# the hashtag is a marker that makes antything written 
+ 
+ # behind it invisible for the computers   
+ 
+ #   integer are whole numbers without decimal points as we dont need any special symbols we can write the number as is.
+ 
+ 10 
+ 
+ 20  
+
+ 2.5 
+
+ -4 
+  
+  Floats-- are a type to represent decimals  
+ expls, 
+
+ 4.0 
+ 3.7 
+ 2.99887625652534567   
+
+ Boolean-- The data type is a value that can only be True or false 
+
+ examples,
+  True 
+
+  False 
