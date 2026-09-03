f7086e4d33e1b54e99b19f0125b244b3aba00c45
A2shifty07 <amaddox354@boyslatin.net>
2026-09-03 13:44:31 +0000

    updated classwork

---
+++ variableActivity.py
@@ -0,0 +1,42 @@
+# VARIABLES - a container that stores data you can name your containter almost anything you want, so long as it is descriptive. 
+
+Name = "Ahmad maddox" 
+yearsofExp= 0
+school= 10th grade 
+
+# when we want to create a variable, we make up a word, we use the equal sign and then assign it is a data type. 
+# whenever  the computer sees our varible word it will always use the data inside  
+
+ example Douglas42= 
+age=10 
+Age=10 
+AGE=10  
+# When we want to create a variable, we make up a word, 
+# use the equal sign and then assign it a data type.  
+# whenever the computer sees our variable word it will
+# alway use the data inside. 
+Variable naming rules 
+ 
+ # 1. a variable must start with a letter and cannot start with a number 
+ 
+ # 2. a variable can be mixed of letters but not symbols 
+ 
+ # 3. a variable cannot be a phython keywords 
+
+ numberofapplesinthesupermarket= 250 
+integer
+numberofstudentsinamathclass= 14 
+integer 
+Travelinsurance= True or false 
+booleans 
+bronzesilvergold= 
+diffrentdrinks3=
+ 
+ # camel casreating variable names with multiple words every new word begins with a capital latter the reason why its called camel case is bc every new word represents a hump. 
+ usernamefornewtiktokaccount = 0 
+ passwordrecoverytiktokaccount = 0 
+
+# snake case- when creating a varaible using snake case we use a underscore symbol to space out each new word the reason why its called snake case is bc the underscore represents a sanke on the ground. new_word_ 
+
+Username_for_new_instagram_accounts = 0
+password_recovery_tiktok_account = 0
