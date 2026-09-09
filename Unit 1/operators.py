2c1b6d96917bf1e56a07c5a6ebbf3bafa3fcc3f9
A2shifty07 <amaddox354@boyslatin.net>
2026-09-09 13:44:32 +0000

    notes for today

---
+++ Unit 1/operators.py
@@ -0,0 +1,29 @@
+# Assignment Operators - Exclusively used to 
+# assign values to variables. 
+# think key / vaule pairings 
+
+# we use a single equal sign to represent the assignment 
+# operator 
+ 
+name = 'Ahmad' 
+grade = 10 
+school = True  
+
+#Arithmetic operators - used on numerical 
+ # data types  to peform claculations. 
+ # Integers(whole numbers) and floats ( decimal numbers) 
+
+# Print is a function that lets us show code 
+# in the terminal 
+
+print(3+3) # addition operator 
+print(3-3) # subtraction operator 
+print(12/3) # division operator 
+print(3 * 4) # muliplication operator  
+
+# comparison operators - set of symbols used  
+# to asses if data is the same or diffrent and how they differ. 
+
+print(10 > 1) # greater that operator 
+print(10 < 2) # less than operator  
+print("ahmad" ==  "Ahmad")  # same as 
